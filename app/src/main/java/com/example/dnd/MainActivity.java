08bc97fe6086cc2d7ffda0c5f2cfa734237e6bdf
Grace Pelella <gmpelella@wpi.edu>
2020-02-20 17:12:08 -0500

    start

---
+++ app/src/main/java/com/example/dnd/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dnd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //DND START
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
