500f1f929c15e8df6d39cea8865059db3448681a
mcfattermarc <mcfattermarc@gmail.com>
2020-03-02 00:54:05 -0500

    Buttons changed, Grace's Die Roller Activity created, pushing fixed menus now

--- app/src/main/java/com/example/dnd/MainActivity.java
+++ app/src/main/java/com/example/dnd/MainActivity.java
@@ -17,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
+    public void launchDieRoller(View view) {
+        Intent intent = new Intent(this, DieRollerActivity.class);
+        startActivity(intent);
+    }
+
     public void launchCreateChar(View view) {
         Intent intent = new Intent(this, CreateCharActivity.class);
         startActivity(intent);
@@ -37,9 +41,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ExportCharActivity.class);
         startActivity(intent);
     }
-=======
-    //ROLL DICE TIME CLACULATOR
-    //grace job
 
->>>>>>> c3322e2ab63f3d03732f85587e3133be7992ea76
 }
